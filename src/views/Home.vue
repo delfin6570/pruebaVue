f70059c6766a41c0ed9b930c0ff5bcafc0033e63
delfin6570 <Carmen1972@outlook.es>
2022-03-30 09:09:58 -0600

    main

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,10 @@
 <template>
   <div>
-    <b-card title=" ">
-      <b-card title="">
-        <b-card-title />
-        <b-card-text />
-        <table-basic />
-        <secon-page />
-      </b-card>
+    <b-card title="">
+      <b-card-title />
+      <b-card-text />
+      <table-basic />
+      <secon-page />
     </b-card></div>
 </template>
 
--- src/views/SecondPage.vue
+++ src/views/SecondPage.vue
@@ -8,7 +8,7 @@
           :height="40"
         />
         <b-img
-          :src="require('/home/delfin/Documentos/proyectosVue/pruebaVue/src/assets/images/icons/index.jpeg')"
+          :src="require('/home/delfin/Escritorio/vuexy-full-version/src/assets/images/icons/index.jpeg')"
           alt="Image 1"
           :height="40"
         />
@@ -124,14 +124,73 @@
               >
                 Enviar</b-button>
               <div>
-                <b-card
-                  footer="Card Footer"
-                  footer-tag="footer"
-                  footer-bg-variant="danger"
-                  footer-border-variant=""
-                  title=""
-                  style="max-width: 20rem;"
-                />
+                <b-row>
+                  <b-col>
+
+                    <b-img
+                      :src="require('@/assets/images/icons/images.png')"
+                      alt="Image 1"
+                      :height="40"
+                      align-h:left
+                    />
+                    <b-img
+                      :src="require('/home/delfin/Escritorio/vuexy-full-version/src/assets/images/icons/index.jpeg')"
+                      alt="Image 1"
+                      :height="40"
+                      align-h="left"
+                    />
+                    <b-row>
+                      <b-col>Dirección General de Seguridad Privada </b-col>
+                      <b-col>Dirección: Calle Ermita 5, Piso 2,
+                        Colonia Narvarte Poniente,
+                        Alcaldía Benito Juárez, C.P. 03020,
+                        Ciudad de México.
+                      </b-col>
+                      <b-col>Teléfono:
+                        52425100 Ext 5267, 5258
+                      </b-col>
+                      <b-col>Correo electrónico:
+                        dgsegpriv01@ssp.cdmx.gob.mx
+                        masuarez@ssp.cdmx.gob.mx
+                      </b-col>
+                      <b-col>Redes Sociales
+                        <b-img
+                          :src="require('/home/delfin/Escritorio/vuexy-full-version/src/assets/images/icons/face.png')"
+                          alt="Image 1"
+                          :height="40"
+                          align-h="left"
+                        />
+                        <b-img
+                          :src="require('/home/delfin/Escritorio/vuexy-full-version/src/assets/images/icons/youtube.png')"
+                          alt="Image 2"
+                          :height="40"
+                          align-h="left"
+                        />
+                        <b-img
+                          :src="require('/home/delfin/Escritorio/vuexy-full-version/src/assets/images/icons/instagram.jpeg')"
+                          alt="Image 2"
+                          :height="40"
+                          align-h="left"
+                        />
+                        <b-img
+                          :src="require('/home/delfin/Escritorio/vuexy-full-version/src/assets/images/icons/twitter22.png')"
+                          alt="Image 2"
+                          :height="40"
+                          align-h="left"
+                        />
+                      </b-col>
+                      <b-col>Sitios Relacionados
+                      </b-col>
+
+                      <b-col>
+                        <a href="#">Portal de la Secretaría de Seguridad Ciudadana </a>
+
+                        <a href="#">Empresas registradas para servicio de Seguridad Privada </a>
+                        <a href="#">Trámites en Línea </a>
+                        <a href="#">Estrado Electrónico  </a>
+                      </b-col>
+
+                    </b-row></b-col></b-row>
               </div>
             </b-row>
           </div>
@@ -154,6 +213,7 @@ import {
   BFormFile,
   BImg,
   BButton,
+  // BIcon,
   // BNnavbar,
   // BAlert,
   // BContainer,
@@ -171,6 +231,7 @@ export default {
     BFormFile,
     BImg,
     BButton,
+    // BIcon,
     // BNnavbar,
     // BAlert,
     // BContainer,
